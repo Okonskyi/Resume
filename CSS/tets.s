e3c7e0353c62d33bfc2e3eb47ec19067d4118f4a
Okonskyi <a.okonskiy@gmail.com>
2021-12-08 22:00:49 +0200

    Залил с нуля резюме

---
+++ CSS/tets.s
@@ -0,0 +1,49 @@
+body { 
+    font-family: 'Montserrat', sans-serif;
+    background-color: #F5F7FA;
+}
+
+.main-container {
+    display:flex;
+    flex-direction: row-reverse;
+    width: 1200px;
+    background-color: #fff;
+    margin: 40px auto;
+}
+
+.about-me-profession {
+font-size: 14px;
+font-weight: 700;
+color: #000;
+}
+
+.about-me-section {
+    background: rgb(245, 245, 146);
+    display: flex;
+    flex-direction: column;
+}
+
+.my-projects-section{
+    background: rgb(146, 189, 245);
+}
+
+.my-work-experience-section {
+    background: rgb(204, 146, 245);
+}
+
+.my-education-section {
+    background: rgb(192, 245, 146);
+}
+
+sidebar-section {
+    background: #1e2939;
+}
+
+li {
+    background-color: #fff;
+    font-size: 14px;
+    line-height: 24px;
+}
+
+project-items-first{
+    color: #f9dd0c;
